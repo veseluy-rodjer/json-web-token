ac10d6bb5bbbed328f208e5fa6d3b5dd460948ac
Nikolay <nikolas-ja@meta.ua>
2022-02-17 02:38:50 +0200

    fix getData method

--- src/Services/JsonWebTokenService.php
+++ src/Services/JsonWebTokenService.php
@@ -11,10 +11,10 @@ class JsonWebTokenService
     {
         $header = json_encode(['alg' => self::ALG, 'type' => self::TYPE_TOKEN]);
         $payload = json_encode($data['payload']);
-        $unsignedToken = $this->base64UrlEncode($header) + '.' + $this->base64UrlEncode($payload);
+        $unsignedToken = $this->base64UrlEncode($header) . '.' . $this->base64UrlEncode($payload);
         $signature = hash_hmac(self::ALG, $unsignedToken, $data['secret']);
 
-        return $unsignedToken + '.' + $this->base64UrlEncode($signature);
+        return $unsignedToken . '.' . $this->base64UrlEncode($signature);
     }
 
     public function base64UrlEncode(string $data): bool|string
@@ -42,9 +42,9 @@ class JsonWebTokenService
         $array = explode('.', $token);
         $signature = $this->base64UrlDecode($array[2]);
 
-        $unsignedToken = $array[0] + '.' + $array[1];
+        $unsignedToken = $array[0] . '.' . $array[1];
         if ($signature === hash_hmac(self::ALG, $unsignedToken, $secret)) {
-            return json_decode($this->base64UrlDecode($array[1]));
+            return json_decode($this->base64UrlDecode($array[1]), true);
         }
 
         return false;
